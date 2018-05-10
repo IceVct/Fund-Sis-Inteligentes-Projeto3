1af513cb8883acdfd57c60fdec9c58864d0313fa
icevct <icevct@gmail.com>
2018-05-10 16:28:19 -0300

    commit do svm

--- projeto3.m
+++ projeto3.m
@@ -2,6 +2,9 @@
 % Alvaro Torres Vieira - 
 % Victor Araujo Vieira - 14/0032801
 
+close all;
+clear all;
+clc;
 
 %% Le os arquivos csv que contem os dados de treino e teste
 tabTreino = readtable('./TreinoTeste/train.csv');
@@ -16,3 +19,40 @@ tabTeste = readtable('./TreinoTeste/test.csv');
 labelsTreino = tabTreino{:, 2};
 valoresTreino = tabTreino{:, 3:end};
 valoresTeste = tabTeste{:, 2:end};
+
+[n,p] = size(valoresTreino)
+isLabels = unique(labelsTreino);
+nLabels = numel(isLabels);
+tabulate(categorical(labelsTreino));
+
+%% SVM
+firstTimeSVM = 0;
+if(firstTimeSVM == 1)
+    [modeloSVM, respostaSVM, rendimentoKFold] = svm(valoresTreino, labelsTreino, valoresTeste);
+    save('modeloSVM.mat', 'modeloSVM');
+    save('rendimentoKFold.mat', 'rendimentoKFold');
+    save('respostaSVM.mat', 'respostaSVM');
+else
+    load('modeloSVM.mat');
+    load('rendimentoKFold.mat');
+    load('respostaSVM.mat');
+end
+
+%% Calculando o numero total de predicoes de cada classe
+[a b c] = unique(respostaSVM);
+histograma = hist(c,length(a));
+fid = fopen('respostaSVMOvOLinear.txt', 'w');
+fid2 = fopen('diferentesDe6OvOLinear.txt', 'w');
+numDif6 = 0;
+totalErros = 0;
+for i = 1:length(a)
+    fprintf(fid, '%s: %d\n', a{i, 1}, histograma(i));
+    if(histograma(i) ~= 6)
+        fprintf(fid2, '%s: %d\n', a{i, 1}, histograma(i));
+        totalErros = totalErros + histograma(i);
+        numDif6 = numDif6 + 1;
+    end
+end
+
+fclose(fid);
+fclose(fid2);
---
+++ svm.m
@@ -0,0 +1,27 @@
+function [modeloSVM, respostaSVM, rendimentoKFold] = svm(valoresTreino, labelsTreino, valoresTeste)
+% Funcao que treina e testa o modelo SVM
+
+rng(1);
+
+%% Treino do modelo
+t = templateSVM('Standardize',1, 'KernelFunction', 'linear'); % normaliza os dados
+CPU = 1;
+if(CPU == 1)
+    options = statset('UseParallel',true);
+    modeloSVM = fitcecoc(valoresTreino, labelsTreino, 'Coding', 'onevsone', 'Learners', t, 'Options', options);
+    modeloCV = crossval(modeloSVM, 'KFold', 10, 'Options', options);
+else
+    modeloSVM = fitcecoc(valoresTreino, labelsTreino, 'Coding','onevsall', 'Learners', t);
+    modeloCV = crossval(modeloSVM, 'KFold', 10);
+end
+
+
+%% Crossvalidation do modelo
+erroEstimado = kfoldLoss(modeloCV);
+rendimentoKFold = 100 - 100*erroEstimado;
+
+% Vendo as respostas do modelo com os dados de teste
+respostaSVM = predict(modeloSVM, valoresTeste);
+
+end
+
